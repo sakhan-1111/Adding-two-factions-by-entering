76144b9935a93f00eaab58e8ad4e56c2b0a3b1ec
Shafiqul Alam Khan <118207040+sakhan-1111@users.noreply.github.com>
2023-12-19 14:35:08 -0600

    Create Adding two factions by entering.c

---
+++ Adding two factions by entering.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    float x,y,sum;
+    printf("Enter the first number = ");
+    scanf("%f", &x);
+    printf("Enter the second number = ");
+    scanf("%f", &y);
+    sum=x+y;
+    printf("The result is = %f",sum);
+    return 0;
+}
